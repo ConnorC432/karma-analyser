e42264ae2e358d3b53a86b23dde230aa19274837
ConnorC432 <connor.chetham@gmail.com>
2025-06-30 23:14:19 +0100

    tiny bit of refactoring

--- cogs/commands.py
+++ cogs/commands.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from ollama import Client
 from .utils import get_gambling_rewards, reddiquette
 
-class GeneralCommands(commands.Cog):
+class Commands(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
@@ -48,10 +48,6 @@ class GeneralCommands(commands.Cog):
     @commands.command()
     async def gild(self, ctx):
         await ctx.reply("Thank you kind stranger!")
-
-    @commands.command()
-    async def help(self, ctx):
-        await ctx.reply("https://tenor.com/view/no-gif-24966265")
 
     @commands.command()
     @commands.has_role("Karma Court Judge")
@@ -156,4 +152,4 @@ class GeneralCommands(commands.Cog):
         await ctx.reply(clean_response[:2000])
 
 async def setup(bot):
-    await bot.add_cog(GeneralCommands(bot))
+    await bot.add_cog(Commands(bot))
