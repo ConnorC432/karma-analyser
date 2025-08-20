ab87cfdefa8d1090a1e3770de973f5f33b35f301
ConnorC432 <connor.chetham@gmail.com>
2025-08-20 12:58:31 +0100

    more accurate r/diagnosis

--- cogs/commands.py
+++ cogs/commands.py
@@ -125,7 +125,7 @@ class Commands(commands.Cog):
         reply = await ctx.reply("DIAGNOSING...")
         message_log = []
         async for msg in ctx.channel.history(limit=200):
-            if msg.author == user:
+            if msg.author == user and "r/" not in msg.content:
                 message_log.append(msg.content)
 
         ai_instructions = ("You are a reddit moderation bot...\n" + reddiquette)
