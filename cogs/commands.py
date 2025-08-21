33f9ec2678832aa1995a95c32362f202d5ae3062
ConnorC432 <connor.chetham@gmail.com>
2025-08-21 13:17:31 +0100

    r/gifs

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,3 +1,5 @@
+from urllib import parse, request
+
 import discord
 import asyncio
 import json
@@ -190,6 +192,27 @@ class Commands(commands.Cog):
         )
         clean_response = re.sub(r"<think>.*?</think>\\n\\n", "", response.message.content, flags=re.DOTALL)
         await ctx.reply(clean_response[:2000])
+
+    @commands.command(aliases=["gif", "pic", "pics", "picture", "pictures"])
+    async def gifs(self, ctx, *, text: str):
+        with open("settings.json", "r") as f:
+            settings = json.load(f)
+            giphy_key = settings.get("giphy_key")
+
+        giphy_url = "https://api.giphy.com/v1/gifs/search"
+
+        params = parse.urlencode({
+            "q": text,
+            "api_key": giphy_key,
+            "limit": 5
+        })
+
+        with request.urlopen(f"{giphy_url}?{params}") as response:
+            data = json.loads(response.read())
+
+        gif_urls = [item['images']['original']['url'] for item in data['data']]
+
+        await ctx.message.reply(random.choice(gif_urls))
 
 
 async def setup(bot):
