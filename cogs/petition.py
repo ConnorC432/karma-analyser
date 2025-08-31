3f2e701b0ef6c598c91ac0c9a628c74857ec7abe
ConnorC432 <connor.chetham@gmail.com>
2025-08-31 15:39:37 +0100

    r/petition

---
+++ cogs/petition.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+
+class Petition(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def petition(self, ctx, *, text: str):
+        """
+        Create a petition
+        - `text` (required): The petition text
+        """
+        reply = await ctx.message.reply(content=text, file=discord.File("./petition.gif"))
+
+        await reply.add_reaction("🖊️")
+
+
+async def setup(bot):
+    await bot.add_cog(Petition(bot))
