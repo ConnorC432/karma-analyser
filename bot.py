2f5288b6b19053cbdf6a29bd6c18d46aed00042e
ConnorC432 <connor.chetham@gmail.com>
2025-09-09 08:57:45 +0100

    use arguments to specify which cogs to load

--- bot.py
+++ bot.py
@@ -2,7 +2,13 @@ import discord
 import json
 import os
 import asyncio
+import argparse
 from discord.ext import commands
+
+# Parse launch arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("-c", nargs="*", default=[], help="The cogs to load")
+args = parser.parse_args()
 
 # Load settings
 with open("settings.json", "r") as f:
@@ -18,13 +24,21 @@ async def on_ready():
 
 # Load cogs
 async def load_extensions():
-    for filename in os.listdir("./cogs"):
-        if filename.endswith(".py") and not filename.startswith("_") and filename != "utils.py":
+    if args.c:
+        for cog in args.c:
             try:
-                await bot.load_extension(f"cogs.{filename[:-3]}")
-                print(f"LOADED COG: {filename}")
+                await bot.load_extension(f"cogs.{cog}")
+                print(f"LOADED COG: {cog}")
             except Exception as e:
-                print(f"FAILED TO LOAD COG: {filename}: {e}")
+                print(f"FAILED TO LOAD COG: {cog}: {e}")
+    else:
+        for filename in os.listdir("./cogs"):
+            if filename.endswith(".py") and not filename.startswith("_") and filename != "utils.py":
+                try:
+                    await bot.load_extension(f"cogs.{filename[:-3]}")
+                    print(f"LOADED COG: {filename}")
+                except Exception as e:
+                    print(f"FAILED TO LOAD COG: {filename}: {e}")
 
 async def main():
     async with bot:
