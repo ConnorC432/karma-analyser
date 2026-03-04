66a25d7c2e707e24cff4dda3a8aa668fe3cecd8f
ConnorC432 <connor.chetham@gmail.com>
2026-03-04 08:45:31 +0000

    fixed reaction events giving AttributeErrors

--- cogs/analyse.py
+++ cogs/analyse.py
@@ -135,7 +135,7 @@ class Analyse(commands.Cog):
         async with karma_lock:
             # Count Reactions
             guild_id = payload.guild_id
-            author_id = payload.author_id
+            author_id = payload.message_author_id
 
             karmic_dict[guild_id][author_id][payload.emoji.name] += 1
             karmic_dict[guild_id][author_id]["Karma"] += reaction_dict[payload.emoji.name]
@@ -158,7 +158,7 @@ class Analyse(commands.Cog):
         async with karma_lock:
             # Count Reactions
             guild_id = payload.guild_id
-            author_id = payload.author_id
+            author_id = payload.message_author_id
 
             karmic_dict[guild_id][author_id][payload.emoji.name] -= 1
             karmic_dict[guild_id][author_id]["Karma"] -= reaction_dict[payload.emoji.name]
