4939e72289b0dd5ddd23979bc508742a300e6fe9
ConnorC432 <ConnorC432@pm.me>
2026-04-09 13:32:42 +0100

    ask cog template

---
+++ cogs/_asktemplate.py
@@ -0,0 +1,26 @@
+from discord.ext import commands
+
+from askbot import AskCog
+
+
+class AskTemplate(AskCog):
+
+    def __init__(self, bot):
+        system_instructions = {
+            "role"   : "system",
+            "content": (
+            )
+        }
+        super().__init__(bot, "template", system_instructions, valid_server_ids=[])
+
+    @commands.command(hidden=True)
+    async def asktemplate(self, ctx, *, text: str):
+        """
+        Ask the Karma Analyser questions
+        - `text` (required): The question to ask.
+        """
+        await self.run_ask(ctx, text)
+
+
+async def setup(bot):
+    await bot.add_cog(AskTemplate(bot))
