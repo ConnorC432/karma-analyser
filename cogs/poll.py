dded407ec4a2f361a9a458f49a90b56c526764f5
ConnorC432 <ConnorC432@pm.me>
2026-04-09 14:48:20 +0100

    r/poll uses discord poll instead of embed

--- cogs/poll.py
+++ cogs/poll.py
@@ -1,9 +1,8 @@
 import logging
+from datetime import timedelta
 
 import discord
 from discord.ext import commands
-
-from utils import REDDIT_RED, emoji_numbers
 
 
 class Poll(commands.Cog):
@@ -13,14 +12,21 @@ class Poll(commands.Cog):
         self.logger = logging.getLogger(self.__class__.__name__)
 
     @commands.command()
-    async def poll(self, ctx, question: str, *options: str) -> None:
+    async def poll(self, ctx, question: str, duration: float, *options: str) -> None:
         """
         Create a poll with between 2 and 10 questions.
+        Underscores in the options will be replaced with spaces.
         - `text` (required): Poll question
+        - `int`  (required): Poll duration in hours
         - `text` (required): Option 1
         - `text` (required): Option 2
         - `text` (optional): Option 3...
         """
+
+        # Ensure duration is between 1 and 768 hours - Prevents errors
+        duration = round(duration)
+        duration = max(1, min(duration, 768))
+        duration = timedelta(hours=duration)
 
         if len(options) < 2:
             await ctx.message.reply("A poll must have at least 2 options")
@@ -31,16 +37,12 @@ class Poll(commands.Cog):
             self.logger.warning("A poll can't have more than 10 options")
             return
 
-        embed = discord.Embed(
-            title=question,
-            description="\n".join(f"### {emoji_numbers[i]} {opt} \n" for i, opt in enumerate(options)),
-            color=REDDIT_RED
-        )
+        poll = discord.Poll(question=question[:300], duration=duration, multiple=False)
+        for opt in options:
+            opt = opt.replace("_", " ")
+            poll.add_answer(text=opt)
 
-        poll_message = await ctx.message.reply(embed=embed)
-
-        for i in range(len(options)):
-            await poll_message.add_reaction(emoji_numbers[i])
+        await ctx.message.reply(poll=poll)
 
 
 async def setup(bot):
