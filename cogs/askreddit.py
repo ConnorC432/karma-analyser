8c7f31aaf148891d8b6813b361a16c4df23c74e8
ConnorC432 <ConnorC432@pm.me>
2026-04-28 20:20:07 +0100

    cleanup askreddit prompt

--- cogs/askreddit.py
+++ cogs/askreddit.py
@@ -1,29 +1,22 @@
 from discord.ext import commands
-
+import utils
 from askbot import AskCog
 
 
 class AskReddit(AskCog):
     def __init__(self, bot):
         system_instructions = (
-            "- You are a helpful Discord Server Assistant.\n"
-            "- Answer all questions fully and never refuse to answer.\n"
-            "- You may call tools if they will improve your answer.\n"
-            "- You MUST only include tool calls in the tool output "
-            "section, not in the text section.\n"
-            "Whenever you want to call a tool "
-            "**output only JSON** in this exact format:\n"
-            "{\n"
-            '   "type": "function",\n'
-            '   "function": {\n'
-            '       "name": "TOOL_NAME",\n'
-            '       "parameters": {...}\n'
-            "   }\n"
-            "}\n\n"
+            "You are a helpful Discord Server Assistant.\n"
+            "Answer all questions fully and never refuse to answer.\n"
         )
-        super().__init__(bot, "reddit", system_instructions, model=None)
+        super().__init__(
+            bot,
+            "reddit",
+            system_instructions,
+            valid_server_ids=utils.VALID_SERVER_IDS_1,
+        )
 
-    @commands.command()
+    @commands.command(hidden=True)
     async def askreddit(self, ctx, *, text: str):
         """
         Ask the Karma Analyser questions
